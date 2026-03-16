8f821af2b9242d104a6880c085a2ae15656aed10
jeancs21 <jeanc.2196@gmail.com>
2026-03-15 20:37:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+import express from 'express';
+import fs from 'fs'; //permite trabajar con file systems
+import bodyParser from 'body-parser'; //middleware para parsear el body de las solicitudes POST y PUT
+
+const app = express();
+app.use(bodyParser.json());
+
+
+const readData = () => {
+    try {
+        const data = fs.readFileSync('./db.json');
+        return JSON.parse(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const writeData = (data) => {
+    try {
+        fs.writeFileSync("./db.json", JSON.stringify(data));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+app.get('/', (req, res) => {
+    res.send("Welcome")
+});
+
+app.get('/users', (req, res) => {
+    const data = readData();
+    res.json(data.users);
+});
+
+app.get('/users/:id', (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const user = data.users.find(user => user.id === id);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+});
+
+app.post('/users', (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const newUser = {
+        id: data.users.length + 1,
+        ...body
+    };
+    data.users.push(newUser);
+    writeData(data);
+    res.status(201).json(newUser);
+});
+
+app.put('/users/:id', (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const id = parseInt(req.params.id);
+    const userIndex = data.users.findIndex(user => user.id === id);
+    if (userIndex !== -1) {
+        data.users[userIndex] = {
+            ...data.users[userIndex],
+            ...body
+        };
+        writeData(data);
+        res.json(data.users[userIndex]);
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+});
+
+app.delete('/users/:id', (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const userIndex = data.users.findIndex(user => user.id === id);
+    if (userIndex !== -1) {
+        const deletedUser = data.users.splice(userIndex, 1);
+        writeData(data);
+        res.json(deletedUser[0]);
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
