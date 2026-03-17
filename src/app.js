ce7eabf9453ec258e25bee953ec9ee427777b393
jeancs21 <jeanc.2196@gmail.com>
2026-03-17 19:54:18 -0400

    Moved cors to middleware directory

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 import express from 'express';
-import cors from 'cors';
+import { corsMiddleware } from './middlewares/corsMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(corsMiddleware);
 
 app.use('/', userRoutes);
-
-app.get('/', (req, res) => {
-    res.send("Welcome")
-});
 
 export default app;
